45f82d6dd65cf6e203aadd77036752bc9616c3f5
Robert Rindos <80785339+robertrindos@users.noreply.github.com>
2021-09-05 12:07:03 -0400

    Add files via upload
    updated app.py file

--- app.py
+++ app.py
@@ -1,15 +1,22 @@
+#!pip install tensorflow
+#!pip install numpy
+#!pip install pandas
+
 from flask import Flask
 import pandas as pd
 import numpy as np
 from google.cloud import storage
+import Dask as dd
 
 movies = pd.read_csv('gs://data_bucket_rr1/rotten_tomatoes_movies.csv')
-print(movies.head(5))
 titles = movies['movie_title']
+
 output_info = movies[['movie_title', 'tomatometer_rating', 'directors', 'actors']]
+
 chosen_movie = 'zootopia'
 
 sim_matrix = pd.read_csv('gs://data_bucket_rr1/sim_matrix.csv')
+print(sim_matrix.head())
 
 # Function 1 - Searches 'chosen_movie' from the movie database
 # returns the similarity scores for every movie in relation to 'chosen_movie'
@@ -32,8 +39,35 @@ def top_5_list(sim_list, output_info):
     output = output_info.fillna(' ', inplace=False)
     return output.iloc[top_5_indices].reset_index(drop=True)
 
+sim_list = get_sim_scores(chosen_movie,titles,sim_matrix)
+top_5 = top_5_list(sim_list, output_info)
+print(top_5)
 
 app = Flask(__name__)
+
 @app.route("/")
 def Home():
     return ("Home Page")
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    pass
+
+
+
+# use tensorflow for cosine similarity, https://towardsdatascience.com/super-charged-similarity-metric-calculations-969116abd948
+# use Dask in OG .py for .to_csv() and Dask .read_csv()
+# Save Compact matrix as csv not cosine sim
+# cosine sim will run in this app.py script instead
+@tf.function
+def tf_cosine_similarity(u, v):
+  u = tf.expand_dims(u, 1)
+
+  n = tf.reduce_sum(u * v, axis=2)
+  d = tf.linalg.norm(u, axis=2) * tf.linalg.norm(v, axis=1)
+
+  return n / d
+
+print("tf:", "\n", tf_cosine_similarity(x, y).numpy())
